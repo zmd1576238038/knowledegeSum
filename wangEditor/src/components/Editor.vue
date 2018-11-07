c4966bd78ce328f2ff8e2597920548992bc99897
zmd1576238038 <1576238038@qq.com>
2018-11-07 14:11:41 +0800

    vue 富文本编辑

---
+++ wangEditor/src/components/Editor.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div ref="editor" style="text-align:left"></div>
+        <button v-on:click="getContent">查看内容</button>
+    </div>
+</template>
+
+<script>
+    import E from 'wangeditor'
+    export default {
+      name: 'editor',
+      data () {
+        return {
+          editorContent: ''
+        }
+      },
+      methods: {
+        getContent: function () {
+            alert(this.editorContent)
+        }
+      },
+      mounted() {
+        var editor = new E(this.$refs.editor)
+        editor.customConfig.onchange = (html) => {
+          this.editorContent = html
+        }
+        editor.customConfig.uploadImgShowBase64 = true  ; // 使用 base64 保存图片
+        editor.customConfig.uploadImgMaxSize = 3 * 1024 * 1024;
+        editor.customConfig.uploadImgMaxLength = 5;
+
+        editor.create()
+      }
+    }
+</script>
+
+<style scoped>
+</style>
